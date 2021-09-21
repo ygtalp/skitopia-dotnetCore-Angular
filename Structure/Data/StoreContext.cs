78117b2af300cf2fb136f2d5a5362539b82af896
ygtalp <unal.yigitalp@gmail.com>
2021-09-22 02:50:02 +0300

    Second commit desktop

---
+++ Structure/Data/StoreContext.cs
@@ -0,0 +1,14 @@
+using Kernel.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Structure.Data
+{
+    public class StoreContext : DbContext
+    {
+        public StoreContext(DbContextOptions<StoreContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Product> Products { get; set; }
+    }
+}
